5e12bbc2e5d6e2b5846be0458680ebd945c0e409
peter <petrel.harp@gmail.com>
2018-12-20 23:15:49 -0800

    spreading ancestry

--- plots/plot_density.py
+++ plots/plot_density.py
@@ -1,4 +1,4 @@
-import pyslim, msprime
+import pyslim, msprime, tsinfer
 import numpy as np
 import spatial_slim as sps
 
@@ -11,7 +11,10 @@ import matplotlib.collections as cs
 import importlib
 importlib.reload(sps)
 
-ts = sps.SpatialSlimTreeSequence(pyslim.load("flat_map.23.trees"), dim=2)
+treefile = "flat_map.300.23.trees"
+outbase = ".".join(treefile.split(".")[:-1])
+
+ts = sps.SpatialSlimTreeSequence(pyslim.load(treefile), dim=2)
 
 circle = ts.individuals_in_circle(center=[4,5], radius=2)
 colors = ['red' if x else 'black' for x in np.isin(range(ts.num_individuals), circle)]
@@ -78,14 +81,15 @@ def animate_tree(ts, children, outfile):
     ax.set_ylim(0, ymax)
     # colors
     colormap = lambda x: plt.get_cmap("cool")(x/max(ts.individual_ages))
+    # treecolors = [plt.get_cmap("viridis")(x) for x in np.linspace(0, 1, len(children))]
     locs = ts.individual_locations
     births = ts.individual_times
-    inds = ts.individuals_by_time(ts.slim_generation)
+    inds = ts.individuals_by_time(0)
     circles = ax.scatter(locs[inds, 0], locs[inds, 1], s=10, 
                          edgecolors=colormap([0 for _ in inds]),
                          facecolors='none')
     paths = []
-    lc = cs.LineCollection(paths, colors='black', linewidths=0.5)
+    lc = cs.LineCollection(paths, linewidths=0.5)
     ax.add_collection(lc)
 
     def update(frame):
@@ -107,7 +111,51 @@ def animate_tree(ts, children, outfile):
                                   frames=np.linspace(0, ts.slim_generation, ts.slim_generation + 1))
     animation.save(outfile, writer='ffmpeg')
 
+def animate_ancestry(ts, children, outfile):
+    # an animation of how the ancestry of an individual spreads out over space
+    # note that this doesn't look quite right because parents have the child's mass
+    # even *after* the child is born
+    fig = plt.figure(figsize=(9,9))
+    ax = fig.add_subplot(111)
+    xmax = max([ind.location[0] for ind in ts.individuals()])
+    ymax = max([ind.location[1] for ind in ts.individuals()])
+    ax.set_xlim(0, xmax)
+    ax.set_ylim(0, ymax)
+    # colors
+    colormap = lambda x: plt.get_cmap("cool")(x/max(ts.individual_ages))
+    locs = ts.individual_locations
+    births = ts.individual_times
+    # find the mean proportion of the children's genome that each node is parental to
+    child_nodes = [n for ind in children for n in ts.individual(ind).nodes]
+    node_ancestry = ts.proportion_ancestry_nodes([child_nodes])[0]
 
-plot_individuals(ts, "locations.pdf")
-animate_individuals(ts, "pop.mp4")
-animate_tree(ts, np.random.choice(np.where(ts.individual_times == 0)[0], 10), "trees.mp4")
+    def size_fun(inds, scale=2000):
+        return np.fromiter(map(lambda x: 1 + scale * sum(node_ancestry[ts.individual(x).nodes]), inds),
+                           'float')
+
+    inds = ts.individuals_by_time(0)
+    circles = ax.scatter(locs[inds, 0], locs[inds, 1], 
+                         sizes=size_fun(inds),
+                         color=colormap([0 for _ in inds]),
+                         alpha=0.75)
+    def update(frame):
+        inds = ts.individuals_by_time(frame)
+        circles.set_offsets(locs[inds,:])
+        # color based on age so far
+        circles.set_color(colormap(births[inds] - frame))
+        circles.set_sizes(size_fun(inds)),
+        return circles
+
+    animation = ani.FuncAnimation(fig, update, 
+                                  frames=np.linspace(0, ts.slim_generation - 1, ts.slim_generation))
+    animation.save(outfile, writer='ffmpeg')
+
+
+today = np.where(ts.individual_times == 0)[0]
+
+if True:
+    plot_individuals(ts, outbase + ".locations.pdf")
+    animate_individuals(ts, outbase + ".pop.mp4")
+    animate_tree(ts, np.random.choice(today, 10), outbase + ".trees.mp4")
+
+animate_ancestry(ts, np.random.choice(today, 1), outbase + ".ancestry.mp4")
--- plots/spatial_slim/__init__.py
+++ plots/spatial_slim/__init__.py
@@ -1,5 +1,6 @@
 import pyslim, msprime
 import numpy as np
+import tqdm
 
 
 class SpatialSlimTreeSequence(pyslim.SlimTreeSequence):
@@ -23,7 +24,7 @@ class SpatialSlimTreeSequence(pyslim.SlimTreeSequence):
 
         return np.sqrt(np.sum((self.individual_locations - point) ** 2, axis=1))
 
-    def individuals_in_circle(self, center, radius):
+    def individuals_in_circle(self, center, radius, time=None):
         """
         Returns the IDs of individuals at distance less than or equal to `radius` of `center.
 
@@ -31,18 +32,24 @@ class SpatialSlimTreeSequence(pyslim.SlimTreeSequence):
         :param float radius: The radius of the circle.
         """
         dists = self.individual_distance_to_point(center)
-        return np.where(dists <= radius)[0]
+        these = np.logical_and(dists <= radius, self.individuals_alive(time))
+        return np.where(these)[0]
 
     def individuals_alive(self, time):
         """
         Returns a logical array of length equal to the number of individuals that
         indicates whether the given individual was alive at the given time.
 
-        :param float time: The time ago.
+        :param float time: The time ago. If `time` is None, then everyone will be
+            counted as being alive.
         """
-        births = self.individual_times
-        ages = self.individual_ages
-        return np.logical_and(births >= time, births - ages <= time)
+        if time is None:
+            out = np.repeat(True, self.num_individuals)
+        else:
+            births = self.individual_times
+            ages = self.individual_ages
+            out = np.logical_and(births >= time, births - ages <= time)
+        return out
 
     def individuals_by_time(self, time):
         """
@@ -99,15 +106,11 @@ class SpatialSlimTreeSequence(pyslim.SlimTreeSequence):
             raise ValueError("Individual child index out of bounds.")
         child_nodes = [x for y in children for x in self.individual(y).nodes]
         node_parents = self.get_node_parents(child_nodes, left=left, right=right)
-        if time is not None:
-            alive = self.individuals_alive(time)
-        else:
-            alive = np.repeat(True, self.num_individuals)
+        alive = self.individuals_alive(time)
         out = [(self.node(a).individual, self.node(b).individual) for a, b in node_parents]
         return [(a, b) for (a, b) in out 
                 if a is not msprime.NULL_INDIVIDUAL and b is not msprime.NULL_INDIVIDUAL
                    and alive[a]]
-
     @property
     def individual_times(self):
         return np.fromiter(map(lambda x: x.time, self.individuals()), 'float')
@@ -125,3 +128,107 @@ class SpatialSlimTreeSequence(pyslim.SlimTreeSequence):
         locations = self.tables.individuals.location
         locations.shape = (int(len(locations)/3), 3)
         return locations[:,:self.dim]
+
+    def proportion_ancestry_nodes(self, sample_sets, show_progress=False):
+        """
+        Computes for each node the proportion of the genomes in each of sample sets
+        inheriting from that node.
+        """
+        # Check the inputs (could be done more efficiently here)
+        all_samples = set()
+        for sample_set in sample_sets:
+            U = set(sample_set)
+            if len(U) != len(sample_set):
+                raise ValueError("Cannot have duplicate values within set")
+            if len(all_samples & U) != 0:
+                # TODO: is this necessary? (shouldn't be)
+                raise ValueError("Sample sets must be disjoint")
+            all_samples |= U
+
+        K = len(sample_sets)
+        A = np.zeros((K, self.num_nodes))
+        parent = np.zeros(self.num_nodes, dtype=int) - 1
+        sample_count = np.zeros((K, self.num_nodes), dtype=int)
+        last_update = np.zeros(self.num_nodes)
+        total_length = np.zeros(self.num_nodes)
+
+        def update_counts(edge, sign):
+            # Update the counts and statistics for a given node. Before we change the
+            # node counts in the given direction, check to see if we need to update
+            # statistics for that node. When a node count changes, we add the
+            # accumulated statistic value for the span since that node was last updated.
+            v = edge.parent
+            while v != -1:
+                if last_update[v] != left:
+                    total = np.sum(sample_count[:, v])
+                    if total != 0:
+                        length = left - last_update[v]
+                        for j in range(K):
+                            A[j, v] += length * sample_count[j, v]
+                    last_update[v] = left
+                for j in range(K):
+                    sample_count[j, v] += sign * sample_count[j, edge.child]
+                v = parent[v]
+
+        # Set the intitial conditions.
+        for j in range(K):
+            for u in sample_sets[j]:
+                sample_count[j][u] = 1
+
+        progress_iter = tqdm.tqdm(
+            self.edge_diffs(), total=self.num_trees, disable=not show_progress)
+        for (left, right), edges_out, edges_in in progress_iter:
+            for edge in edges_out:
+                parent[edge.child] = -1
+                update_counts(edge, -1)
+            for edge in edges_in:
+                parent[edge.child] = edge.parent
+                update_counts(edge, +1)
+
+        # Finally, add the stats for the last tree and normalise by the total
+        # length that each node was an ancestor to > 0 samples.
+        for v in range(self.num_nodes):
+            total = np.sum(sample_count[:, v])
+            if total != 0:
+                length = self.sequence_length - last_update[v]
+                for j in range(K):
+                    A[j, v] += length * sample_count[j, v]
+        # renormalize
+        for j in range(K):
+            A[j,:] /= len(sample_sets[j]) * self.sequence_length
+        return A
+
+    def proportion_ancestry_nodes_slow(self, sample_set, left=0.0, right=None):
+        """
+        Computes an array of values that gives, for each node, the total amount of
+        genome in `sample_set` that descent from that node.
+
+        IS VERY SLOOOOOW
+
+        :param int sample_set: The children.
+        :param float time: The time ago the parent should be alive. Defaults to
+            no constraint.
+        :param float left: The left end of the portion of genome considered.
+            Defaults to zero.
+        :param float right: The right end of the portion of genome considered.
+            Defaults to the sequence length.
+        """
+        n = len(sample_set)
+        if right is None:
+            right = self.sequence_length
+        if left < 0 or right > self.sequence_length or left > right:
+            raise ValueError("Illegal left, right bounds.")
+        trees = self.trees(sample_counts=True, tracked_leaves=sample_set)
+        # first count up how much shared ancestry each node has
+        ancestry = np.zeros(self.num_nodes)
+        for tree in trees:
+            if tree.interval[0] > right:
+                break
+            while tree.interval[1] < left:
+                next
+            tree_span = min(right, tree.interval[1]) - max(left, tree.interval[0])
+            for n in tree.nodes():
+                ancestry[n] += tree.num_tracked_samples(n) * tree_span
+        ancestry /= len(sample_set) * ts.sequence_length
+        return ancestry
+
