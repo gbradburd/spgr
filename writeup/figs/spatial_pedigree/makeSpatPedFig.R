3773e3d1b1633bf0980b5afe06a281a6954e13bf
Gideon <bradburd@msu.edu>
2019-04-26 14:54:36 -0400

    better spatial ped fig

--- writeup/figs/spatial_pedigree/makeSpatPedFig.R
+++ writeup/figs/spatial_pedigree/makeSpatPedFig.R
@@ -32,13 +32,13 @@ if(!file.exists("gen_output/gen_100")){
 gens <- c(100,97)
 
 # pick a focal individual
-goodOnes <- sapply(1:100,
-				function(i){
-					ancs <- getAncestors(focalInd=i,gen1=gens[1],stopGen=gens[2])
-					length(unique(ancs[which(ancs[,2]==gens[2]),1])) == 2^(gens[1]-gens[2])
-				})
-#which(goodOnes)
-i <- 98 #sample(which(goodOnes),1)
+# goodOnes <- sapply(1:100,
+				# function(i){
+					# ancs <- getAncestors(focalInd=i,gen1=gens[1],stopGen=gens[2])
+					# length(unique(ancs[which(ancs[,2]==gens[2]),1])) == 2^(gens[1]-gens[2])
+				# })
+
+i <- 5	#sample(which(goodOnes),1)
 
 # specify the most recent generation simulated
 maxGen <- 100
@@ -46,37 +46,53 @@ maxGen <- 100
 # need to plot everything to get it into the composite 
 #	plot list, then can discard this file and
 #	replot cleanly with plotdev()
+
+
+p.cols <- color.func((gens[2]+1):gens[1],c("darkblue","blue","deepskyblue"))
+m.cols <- color.func((gens[2]+1):gens[1],c("darkred","red","coral1"))
+
+getAncCols <- function(ancs,p.cols,m.cols){
+	ancGens <- max(ancs[,2]) - ancs[,2]
+	anc.cols <- numeric(nrow(ancs))
+	anc.cols[1] <- "darkorchid4"
+	for(i in 1:max(ancGens)){
+		anc.cols[2^i:(2^(i+1)-1)] <- c(rep(p.cols[i],2^(i-1)),
+									   rep(m.cols[i],2^(i-1)))
+	}
+	return(anc.cols)
+}
+
 pdf(file="tmp.pdf")
 	ancs <- getAncestors(focalInd=i,gen1=gens[1],stopGen=gens[2])
-	xlim <- range(ancs[,3])
-	ylim <- range(ancs[,4])
-	plotPed(gens=gens[1]:gens[2],xlim=xlim,ylim=ylim,zlim=NULL,maxGen=maxGen)
+	anc.cols <- getAncCols(ancs,p.cols,m.cols)
+	buffer <- 0.02 * c(-1,1)
+	xlim <- range(ancs[,3]) + buffer
+	ylim <- range(ancs[,4]) + buffer
+	plotPed(gens=gens[1]:gens[2],xlim=xlim,ylim=ylim,zlim=NULL,maxGen=maxGen,theta=20,phi=9,r=1000)
+	plotPedLines(ancs,maxGen=maxGen,lwd=3,p.cols=p.cols,m.cols=m.cols)
 	points3D(x=ancs[,3],
 			 y=ancs[,4],
 			 z=maxGen-ancs[,2],
 			 pch=19,
-			 cex=1.2,
-			 col=adjustcolor(2,0.3),
+			 cex=2.4,
+			 col=anc.cols,
+			 colkey=list(plot=FALSE),
+			 colvar=NULL,
 			 add=TRUE)
-	plotPedLines(ancs,maxGen=maxGen,lwd=1)
 dev.off()
-
-# pdf(file="../spatial_pedigree.pdf",width=8,height=8)
-	# plotdev()
-# dev.off()
-
-# pdf(file="../pedigree.pdf",width=8,height=8)
-	# plotNonSpPed(ancs,mar=c(2,0,3,0))
-# dev.off()
 
 
 pdf(file="../spatial_pedigree.pdf",width=16,height=8)
 	par(mfrow=c(1,2))
-	plotNonSpPed(ancs,mar=c(4,0,4,0))
+	plotNonSpPed(ancs,mar=c(4,0,4,0),segLwd=3,pt.cex=2.5)
 	plotdev()
 dev.off()
 
 
-
+# fix time slices perspective
+# fix time slice labeling
+# fix color discrepancy btwn sp and nsp
+# fix time slice color and other inds color
+#	fix associated legend
 
 file.remove("tmp.pdf")
--- writeup/figs/useful_slim_functions.R
+++ writeup/figs/useful_slim_functions.R
@@ -19,23 +19,23 @@ getCoords <- function(x){
 #	with z coordinates determined by the difference 
 #	between the maximum number of generations run 
 #	(aka the present) and the focal generation
-plotGen <- function(coords,z,add=FALSE,xlim=NULL,ylim=NULL,zlim=NULL,maxGen){
+plotGen <- function(coords,z,add=FALSE,xlim=NULL,ylim=NULL,zlim=NULL,maxGen,theta=20,phi=-1,r=1){
 	coords <- coords[-c(which(coords[,1] < xlim[1] | coords[,1] > xlim[2]),
 						which(coords[,2] < ylim[1] | coords[,2] > ylim[2])),]
 	z <- maxGen - z
 	rect3D(x0 = xlim[1],x1 = xlim[2],
 		   y0 = ylim[1],y1 = ylim[2],
 		   z0 = z,
-		   col=adjustcolor(4,0.1),
-		   bty='n',theta=20,phi=-1,
+		   col=adjustcolor("gray",0.3),
+		   bty='n',theta=theta,phi=phi,r=r,
 		   colkey=FALSE,xlab="",ylab="",zlab="",
 		   xlim=xlim,ylim=ylim,zlim=zlim,
 		   add=add)
 	scatter3D(x=coords[,1],
 			  y=coords[,2],
 			  z=rep(z,length(coords[,1])),
-			  pch=19,col=adjustcolor(4,0.1),
-			  cex=0.8,
+			  pch=19,col=adjustcolor("darkgray",0.5),
+			  cex=1.2,
 			  add=TRUE)
 	return(invisible("plotted"))
 }
@@ -44,7 +44,7 @@ plotGen <- function(coords,z,add=FALSE,xlim=NULL,ylim=NULL,zlim=NULL,maxGen){
 #	with z coordinates determined by the difference 
 #	between the maximum number of generations run 
 #	(aka the present) and the focal generation
-plotPed <- function(gens,xlim=NULL,ylim=NULL,zlim=NULL,maxGen){
+plotPed <- function(gens,xlim=NULL,ylim=NULL,zlim=NULL,maxGen,theta=20,phi=-1,r=1){
 	par(mar=c(0,0,0,0))
 	if(is.null(xlim)){
 		xlim <- c(0,1)
@@ -57,7 +57,7 @@ plotPed <- function(gens,xlim=NULL,ylim=NULL,zlim=NULL,maxGen){
 	}
 	x <- getSlimGenOutput(sprintf("gen_output/gen_%s",gens[1]))
 	coords <- getCoords(x)
-	plotGen(coords,z=gens[1],add=FALSE,xlim=xlim,ylim=ylim,zlim=zlim,maxGen=maxGen)
+	plotGen(coords,z=gens[1],add=FALSE,xlim=xlim,ylim=ylim,zlim=zlim,maxGen=maxGen,theta=theta,phi=phi,r=r)
 	lapply(gens[2:length(gens)],
 		function(g){
 			x <- getSlimGenOutput(sprintf("gen_output/gen_%s",g))
@@ -66,7 +66,7 @@ plotPed <- function(gens,xlim=NULL,ylim=NULL,zlim=NULL,maxGen){
 		})
 	text3D(x=mean(xlim)-diff(range(xlim))/6,y=min(ylim)+diff(range(ylim))/9,z=min(zlim),labels=c("longitude"),add=TRUE,cex=1.5)
 #	text3D(x=min(xlim)-diff(range(xlim))/3,y=mean(ylim),z=min(zlim)+diff(range(zlim))/7,labels=c("latitude"),add=TRUE,cex=1.5)
-	text3D(x=max(xlim)+diff(range(xlim))/20,y=min(ylim),z=min(zlim)+diff(range(zlim))/5,labels=c("latitude"),add=TRUE,cex=1.5)
+	text3D(x=max(xlim)+diff(range(xlim))/4.3,y=min(ylim),z=min(zlim)+diff(range(zlim))/6,labels=c("latitude"),add=TRUE,cex=1.5)
 #	text3D(x=max(xlim)+diff(range(xlim))/10,y=min(ylim),z=mean(zlim),labels=c("time"),add=TRUE,cex=1.5)
 #	text3D(x=max(xlim)+diff(range(xlim))/20,y=min(ylim),z=min(zlim)+diff(range(zlim))/5,labels=c("present"),add=TRUE,cex=1.5)
 #	text3D(x=max(xlim)+diff(range(xlim))/20,y=min(ylim),z=max(zlim)-diff(range(zlim))/5,labels=c("past"),add=TRUE,cex=1.5)
@@ -94,7 +94,7 @@ getParentIndices <- function(pedIDs,parentIDs){
 #	of a particular focal individual 
 #	within a specified list of generations
 getAncestors <- function(focalInd,gen1,stopGen){
-	#recover()
+#	recover()
 	nAncs <- sum(2^(0:(gen1-stopGen)))
 	ancsInSpace <- matrix(rep(NA,2*4*nAncs),nrow=nAncs,ncol=4)
 	gen <- gen1
@@ -110,42 +110,81 @@ getAncestors <- function(focalInd,gen1,stopGen){
 		ancsInSpace[ancIndex,] <- matrix(c(x[parIndex,1],
 										   rep(gen,length(parIndex)),
 										   x[parIndex,c(8,9)]),ncol=4)
-		parents <- c(x[parIndex,2:3])
+		parents <- unlist(lapply(parIndex,function(i){x[i,2:3]}))
 	}
 	ancsInSpace <- matrix(as.numeric(ancsInSpace),ncol=4)
 	return(ancsInSpace)
 }
 
-plotPedLines <- function(ancs,maxGen,lwd){
+plotPedLines <- function(ancs,maxGen,lwd,p.cols=1,m.cols=1){
 	#recover()
-	gens <- unique(ancs[,2])
+	gens <- maxGen-unique(ancs[,2])
+	ancs[,2] <- maxGen-ancs[,2]
+	if(m.cols==1 && p.cols==1){
+		p.cols <- rep(1,2^(max(gens)+1)-1)
+		m.cols <- rep(1,2^(max(gens)+1)-1)
+	}
 	invisible(
-		lapply(gens,function(g){
+		lapply(max(gens):1,function(g){
 			if(length(which(ancs[,2] == g)) > 1){
-				nPairs <- length(which(ancs[,2] == g))/2
-				lapply(1:nPairs,
-					function(i){
-						pair <- ancs[ancs[,2]==g,][(i*2-1):(i*2),]
-						child <- ancs[ancs[,2]==(g+1),,drop=FALSE][i,]
-						segments3D(x0=pair[1,3],y0=pair[1,4],z0=maxGen-g,
-									x1=pair[2,3],y1=pair[2,4],z1=maxGen-g,
+				if(g > 1){
+					nPairs <- length(which(ancs[,2] == g))/2
+					lapply(1:nPairs,
+						function(i){
+							seg.col <- ifelse(i <= nPairs/2,p.cols[g],m.cols[g])
+							pair <- ancs[ancs[,2]==g,][(i*2-1):(i*2),]
+							child <- ancs[ancs[,2]==(g-1),,drop=FALSE][i,]
+							segments3D(x0=pair[1,3],y0=pair[1,4],z0=g,
+										x1=pair[2,3],y1=pair[2,4],z1=g,
+										lwd=lwd,
+										lty=2,add=TRUE,col=seg.col)
+							segments3D(x0=mean(pair[,3]),y0=mean(pair[,4]),z0=g,
+										x1=child[3],y1=child[4],z1=g-1,
+										lwd=lwd,col=seg.col,
+										lty=1,add=TRUE)
+					})
+				} else if(g==1) {
+						pair <- ancs[ancs[,2]==g,]
+						child <- ancs[ancs[,2]==(g-1),,drop=FALSE]
+						segments3D(x0=pair[1,3],y0=pair[1,4],z0=g,
+									x1=mean(pair[,3]),
+									y1=mean(pair[,4]),z1=g,
 									lwd=lwd,
-									lty=2,add=TRUE)
-						segments3D(x0=mean(pair[,3]),y0=mean(pair[,4]),z0=maxGen-g,
-									x1=child[3],y1=child[4],z1=maxGen-g-1,
+									lty=2,add=TRUE,col=p.cols[g])
+						segments3D(x0=pair[2,3],y0=pair[2,4],z0=g,
+									x1=mean(pair[,3]),
+									y1=mean(pair[,4]),z1=g,
 									lwd=lwd,
+									lty=2,add=TRUE,col=m.cols[g])
+						segments3D(x0=mean(pair[,3])-0.0003,y0=mean(pair[,4])-0.0003,z0=g,
+									x1=child[3]-0.0002,y1=child[4]-0.0001,z1=g-1,
+									lwd=lwd,col=p.cols[g],
 									lty=1,add=TRUE)
-				})
+						segments3D(x0=mean(pair[,3])+0.0003,y0=mean(pair[,4])+0.0003,z0=g,
+									x1=child[3]+0.0002,y1=child[4]+0.0002,z1=g-1,
+									lwd=lwd,col=m.cols[g],
+									lty=1,add=TRUE)
+				}
 			}
 		})
 	)
 }
 
-plotNonSpPed <- function(ancs,mar=c(3,0,3,0)){
+color.func <- function(x,cols){
+	cols <- colorRampPalette(cols)(length(x))[findInterval(x,sort(x))]
+	return(cols)
+}
+
+plotNonSpPed <- function(ancs,mar=c(3,0,3,0),anc.cols=NULL,segLwd=1.5,pt.cex=2){
+	#recover()
 	nGen <- length(unique(ancs[,2]))
+	if(is.null(anc.cols)){
+		p.cols <- color.func(1:nGen,c("darkblue","blue","deepskyblue"))
+		m.cols <- color.func(1:nGen,c("darkred","red","coral1"))
+	}
 	par(mar=mar)
-	plot(0,type='n',xlim=c(0,1.3),ylim=c(1,nGen+0.2),xlab="",ylab="",main="",xaxt='n',yaxt='n',bty='n')
-	for(n in nGen:1){
+	plot(0,type='n',xlim=c(0,1.4),ylim=c(1,nGen+0.2),xlab="",ylab="",main="",xaxt='n',yaxt='n',bty='n')
+	for(n in nGen:2){
 		if(n == nGen){
 			x <- cumsum(rep((1/(2^(nGen-1)+1)),2^(nGen-1)))
 			y <- rep(nGen,2^(nGen-1))
@@ -153,23 +192,39 @@ plotNonSpPed <- function(ancs,mar=c(3,0,3,0)){
 			x <- (x[seq(1,2^n,by=2)] + x[seq(2,2^n,by=2)])/2
 			y <- rep(n,2^(n-1))
 		}
-		points(x=x,y=y,pch=19,col=adjustcolor("red",0.5),cex=2)
-		if(n > 1){
+		points(x=x,y=y,pch=19,col=c(rep(p.cols[n],(2^n)/4),rep(m.cols[n],(2^n)/4)),cex=pt.cex)
+		if(n > 2){
 			nPairs <- 2^(n-1)/2
 			lapply(1:nPairs,
 				function(i){
-					segments(x0=x[i*2-1],x1=x[i*2],y0=n,y1=n,lty=2)
+					seg.col <- ifelse(i <= nPairs/2,p.cols[n],m.cols[n])
+					segments(x0=x[i*2-1],x1=x[i*2],y0=n,y1=n,lty=2,col=seg.col,lwd=segLwd)
 					segments(x0=mean(c(x[i*2-1],x[i*2])),
 							 x1=mean(c(x[i*2-1],x[i*2])),
 							 y0=n,
-							 y1=n-1,lty=1)
+							 y1=n-1,lty=1,
+							 col=seg.col,lwd=segLwd)
 				})
 		}
+		if(n == 2){
+			segments(x0=x[1],x1=mean(x),y0=n,y1=n,lty=2,col=p.cols[n],lwd=segLwd)
+			segments(x0=x[2],x1=mean(x),y0=n,y1=n,lty=2,col=m.cols[n],lwd=segLwd)
+			segments(x0=mean(x)-0.002,
+					 x1=mean(x)-0.002,
+					 y0=n,
+					 y1=n-1,lty=1,
+					 col=p.cols[n],lwd=segLwd)
+			segments(x0=mean(x)+0.002,
+					 x1=mean(x)+0.002,
+					 y0=n,
+					 y1=n-1,lty=1,
+					 col=m.cols[n],lwd=segLwd)
+		}
 	}
-	text(x=1.2,y=(nGen+1.2)/2,labels="time",cex=2)
-	text(x=1.2,y=1,labels="present",cex=2)
-	text(x=1.2,y=nGen+0.1,labels="past",cex=2)
-	segments(x0=1.2,x1=1.2,y0=1+0.2,y1=(nGen+1)/2-0.1)
-	arrows(x0=1.2,x1=1.2,y0=(nGen+1)/2+0.2,y1=nGen-0.1)
+	points(x=0.5,y=1,col="darkorchid4",cex=pt.cex,pch=19)
+	text(x=1.2,y=1,labels="now",cex=2)
+	text(x=1.2,y=2,labels="parents",cex=2)
+	text(x=1.2,y=3,labels="grandparents",cex=2)
+	text(x=1.2,y=4,labels="great\ngrandparents",cex=2)
 	return(invisible("plotted"))
 }
